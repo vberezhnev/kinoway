54bc7d4f7ef9f325df6ac4942ab3199ac47743fb
Tell396 <vova21473@gmail.com>
2022-08-21 15:15:23 +1000

    Story #15. New series section was added + bugfix.

--- client/src/components/screens/Home/Home.tsx
+++ client/src/components/screens/Home/Home.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import * as React from "react";
 
 import { NewFilms } from "@/components/screens/Home/components/NewFilms/NewFilms"
+import { NewSeries } from "@/components/screens/Home/components/NewSeries/NewSeries"
 
 import styles from "./Home.module.scss";
 
@@ -9,7 +10,8 @@ const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <div className={styles.App}>
-        <NewFilms />
+					<NewFilms />
+					<NewSeries />
       </div>
     </div>
   );
--- client/src/components/screens/Home/components/NewSeries/NewSeries.tsx
+++ client/src/components/screens/Home/components/NewSeries/NewSeries.tsx
@@ -1,7 +1,7 @@
 // Next.js
 import { useRouter } from "next/router";
 
-import { useGetNewFilmsQuery } from "@/services/KinowayService";
+import { useGetNewSeriesQuery } from "@/services/KinowayService";
 import { RoutesEnum } from "@/constants/routes";
 import { useTypedSelector } from "@/hooks/useTypedSelector";
 import { useActions } from "@/hooks/useActions";
@@ -10,33 +10,33 @@ import { useActions } from "@/hooks/useActions";
 import FilmCard from "@/components/FilmCard/FilmCard";
 import Button from "@/components/UI-components/Button/Button";
 
-import styles from "./NewFilms.module.scss";
+import styles from "./NewSeries.module.scss";
 
-export const NewFilms = () => {
+export const NewSeries = () => {
   const { push } = useRouter();
   const { filmsLimit } = useTypedSelector((state) => state.loadReducer);
-  const { data, isFetching } = useGetNewFilmsQuery(filmsLimit);
+  const { data, isFetching } = useGetNewSeriesQuery(filmsLimit);
   const { loadMoreFilms } = useActions();
   const condition = data?.docs?.length === data?.total;
 
   return (
     <section className={styles.main}>
       <div>
-        <div className={styles.NewMovies_top}>
-          <h2>New films</h2>
+        <div className={styles.NewSeries_top}>
+          <h2>New series</h2>
           <Button text="View more" />
         </div>
         <ul>
           {data?.docs?.map((data: Object) => {
-            console.log(`https://kinopoisk.ru/film/${data.id}`);
+            console.log(data);
 							return (
 							<ul>
 								<FilmCard
 										key={data.id}
-									  href={`https://kinopoisk.ru/films/${data.id}`}
+										href={`https://kinopoisk.ru/series/${data.id}`}
 									ratingKP={data.rating.kp}
 									ratingIMDB={data.rating.imdb}
-										src={data.poster.previewUrl}
+									src={data.poster.previewUrl}
 										title={data.name}
 										secondTitle={data.alternativeName}
 								/>
