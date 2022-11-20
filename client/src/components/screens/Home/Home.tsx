9cc6fab7ce5bb5b5451a29cd594324b9d58023b7
Tell396 <vova21473@gmail.com>
2022-11-20 23:49:20 +1000

    Story #68. Adding a snowfall.

--- client/src/components/screens/Home/Home.tsx
+++ client/src/components/screens/Home/Home.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import * as React from "react";
+import Snowfall from 'react-snowfall'
 
 import { NewFilms } from "@/components/screens/Home/components/NewFilms/NewFilms";
 import { NewSeries } from "@/components/screens/Home/components/NewSeries/NewSeries";
@@ -8,7 +9,8 @@ import styles from "./Home.module.scss";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+			<div className={styles.container}>
+					<Snowfall />
       <div className={styles.App}>
         <NewFilms />
         <NewSeries />
