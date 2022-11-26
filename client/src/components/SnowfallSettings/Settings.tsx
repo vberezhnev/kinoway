aaa1911a69fc0cb29c0a71a7ce5ef892762b991f
Tell396 <vova21473@gmail.com>
2022-11-26 22:49:42 +1000

    Story #75. Production bugfix. Yay!

--- client/src/components/SnowfallSettings/Settings.tsx
+++ client/src/components/SnowfallSettings/Settings.tsx
@@ -7,140 +7,140 @@ import Slider from "@mui/material/Slider";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import { CirclePicker } from "react-color";
-import useSettingsStore from "../../pages/snowfall/settings";
+import { useSettingsStore } from "./defaultSettings";
 import { ThemeProvider } from "./theme";
 
 import styles from "./Settings.module.css";
 
 const ValueChip = (props: ChipProps) => {
-  return <Chip {...props} sx={{ ml: 1 }} size="small" component="span" />;
+    return <Chip {...props} sx={{ ml: 1 }} size="small" component="span" />;
 };
 
 const colors = [
-  "#dee4fd",
-  "#e91e63",
-  "#9c27b0",
-  "#673ab7",
-  "#3f51b5",
-  "#2196f3",
-  "#03a9f4",
-  "#00bcd4",
-  "#009688",
-  "#4caf50",
-  "#8bc34a",
-  "#cddc39",
-  "#ffeb3b",
-  "#ffc107",
-  "#ff9800",
-  "#ff5722",
-  "#795548",
-  "#607d8b",
+    "#dee4fd",
+    "#e91e63",
+    "#9c27b0",
+    "#673ab7",
+    "#3f51b5",
+    "#2196f3",
+    "#03a9f4",
+    "#00bcd4",
+    "#009688",
+    "#4caf50",
+    "#8bc34a",
+    "#cddc39",
+    "#ffeb3b",
+    "#ffc107",
+    "#ff9800",
+    "#ff5722",
+    "#795548",
+    "#607d8b",
 ];
 
 export const Settings = () => {
-		const settings = useSettingsStore();
-		//const settings = localStorage.getItem('snowfall');
-		
-		console.log(settings)
-		//localStorage.setItem('snowfall', JSON.stringify(settings))
+    const settings = useSettingsStore();
+    //const settings = localStorage.getItem('snowfall');
 
-  return (
-    <ThemeProvider>
-      <Paper className={styles.settingsContainer}>
-        <Stack spacing={1}>
-          <div>
-            <Typography gutterBottom>
-              Snowflake Count <ValueChip label={settings.snowflakeCount} />
-            </Typography>
-            <Slider
-              value={settings.snowflakeCount}
-              min={0}
-              max={750}
-              step={1}
-              onChange={(_, value) =>
-                settings.update({ snowflakeCount: value as number })
-              }
-            />
-          </div>
-          <div>
-            <Typography gutterBottom>
-              Speed <ValueChip label={`Min ${settings?.speed?.[0]}`} />
-              <ValueChip label={`Max ${settings?.speed?.[1]}`} />
-            </Typography>
-            <Slider
-              value={settings.speed}
-              min={0}
-              max={10}
-              step={0.5}
-              onChange={(_, value) =>
-                settings.update({ speed: value as [number, number] })
-              }
-            />
-          </div>
-          <div>
-            <Typography gutterBottom>
-              Wind <ValueChip label={`Min ${settings?.wind?.[0]}`} />{" "}
-              <ValueChip label={`Max ${settings?.wind?.[1]}`} />
-            </Typography>
-            <Slider
-              value={settings.wind}
-              min={-1}
-              max={10}
-              step={0.5}
-              onChange={(_, value) =>
-                settings.update({ wind: value as [number, number] })
-              }
-            />
-          </div>
-          <div>
-            <Typography gutterBottom>
-              Radius <ValueChip label={`Min ${settings?.radius?.[0]}`} />
-              <ValueChip label={`Max ${settings?.radius?.[1]}`} />
-            </Typography>
-            <Slider
-              value={settings.radius}
-              min={0.5}
-              max={30}
-              step={0.5}
-              onChange={(_, value) =>
-                settings.update({ radius: value as [number, number] })
-              }
-            />
-          </div>
-          {settings.useImages ? (
-            <div>
-              <Typography gutterBottom>
-                Rotation Speed{" "}
-                <ValueChip label={`Min ${settings?.rotationSpeed?.[0]}`} />
-                <ValueChip label={`Max ${settings?.rotationSpeed?.[1]}`} />
-              </Typography>
-              <Slider
-                value={settings.rotationSpeed}
-                min={-5}
-                max={10}
-                step={0.5}
-                onChange={(_, value) =>
-                  settings.update({ rotationSpeed: value as [number, number] })
-                }
-              />
-            </div>
-          ) : (
-            <Box my={2}>
-              <Typography gutterBottom>
-                Color <ValueChip label={settings.color} />
-              </Typography>
-              <CirclePicker
-                colors={colors}
-                width="100%"
-                color={settings.color}
-                onChangeComplete={(value) =>
-                  settings.update({ color: value.hex })
-                }
-              />
-            </Box>
-          )}
-        </Stack>
-      </Paper>
-    </ThemeProvider>
-  );
+    console.log(settings);
+    //localStorage.setItem('snowfall', JSON.stringify(settings))
+
+    return (
+        <ThemeProvider>
+            <Paper className={styles.settingsContainer}>
+                <Stack spacing={1}>
+                    <div>
+                        <Typography gutterBottom>
+                            Snowflake Count <ValueChip label={settings.snowflakeCount} />
+                        </Typography>
+                        <Slider
+                            value={settings.snowflakeCount}
+                            min={0}
+                            max={750}
+                            step={1}
+                            onChange={(_, value) =>
+                                settings.update({ snowflakeCount: value as number })
+                            }
+                        />
+                    </div>
+                    <div>
+                        <Typography gutterBottom>
+                            Speed <ValueChip label={`Min ${settings?.speed?.[0]}`} />
+                            <ValueChip label={`Max ${settings?.speed?.[1]}`} />
+                        </Typography>
+                        <Slider
+                            value={settings.speed}
+                            min={0}
+                            max={10}
+                            step={0.5}
+                            onChange={(_, value) =>
+                                settings.update({ speed: value as [number, number] })
+                            }
+                        />
+                    </div>
+                    <div>
+                        <Typography gutterBottom>
+                            Wind <ValueChip label={`Min ${settings?.wind?.[0]}`} />{" "}
+                            <ValueChip label={`Max ${settings?.wind?.[1]}`} />
+                        </Typography>
+                        <Slider
+                            value={settings.wind}
+                            min={-1}
+                            max={10}
+                            step={0.5}
+                            onChange={(_, value) =>
+                                settings.update({ wind: value as [number, number] })
+                            }
+                        />
+                    </div>
+                    <div>
+                        <Typography gutterBottom>
+                            Radius <ValueChip label={`Min ${settings?.radius?.[0]}`} />
+                            <ValueChip label={`Max ${settings?.radius?.[1]}`} />
+                        </Typography>
+                        <Slider
+                            value={settings.radius}
+                            min={0.5}
+                            max={30}
+                            step={0.5}
+                            onChange={(_, value) =>
+                                settings.update({ radius: value as [number, number] })
+                            }
+                        />
+                    </div>
+                    {settings.useImages ? (
+                        <div>
+                            <Typography gutterBottom>
+                                Rotation Speed{" "}
+                                <ValueChip label={`Min ${settings?.rotationSpeed?.[0]}`} />
+                                <ValueChip label={`Max ${settings?.rotationSpeed?.[1]}`} />
+                            </Typography>
+                            <Slider
+                                value={settings.rotationSpeed}
+                                min={-5}
+                                max={10}
+                                step={0.5}
+                                onChange={(_, value) =>
+                                    settings.update({ rotationSpeed: value as [number, number] })
+                                }
+                            />
+                        </div>
+                    ) : (
+                        <Box my={2}>
+                            <Typography gutterBottom>
+                                Color <ValueChip label={settings.color} />
+                            </Typography>
+                            <CirclePicker
+                                colors={colors}
+                                width="100%"
+                                color={settings.color}
+                                onChangeComplete={(value) =>
+                                    settings.update({ color: value.hex })
+                                }
+                            />
+                        </Box>
+                    )}
+                </Stack>
+            </Paper>
+        </ThemeProvider>
+    );
 };
--- client/src/pages/snowfall/index.tsx
+++ client/src/pages/snowfall/index.tsx
@@ -1,27 +1,27 @@
 import Snowfall from "react-snowfall";
 import { Settings } from "@/components/SnowfallSettings/Settings";
-import useSettingsStore from "./settings";
+import { useSettingsStore } from "@/components/SnowfallSettings/defaultSettings";
 import React, { useEffect } from "react";
 import logo from "./logo.png";
-import styles from './App.module.scss'
+import styles from "./App.module.scss";
 
 const SnowfallPage = () => {
-  const { color, snowflakeCount, radius, speed, wind, useImages } =
-    useSettingsStore();
+    const { color, snowflakeCount, radius, speed, wind, useImages } =
+        useSettingsStore();
 
-  return (
-    <div className={styles.app}>
-      <Snowfall
-        color={color}
-        snowflakeCount={snowflakeCount}
-        radius={radius}
-        speed={speed}
-        wind={wind}
-        images={undefined}
-      />
-      <Settings />
-    </div>
-  );
+    return (
+        <div className={styles.app}>
+            <Snowfall
+                color={color}
+                snowflakeCount={snowflakeCount}
+                radius={radius}
+                speed={speed}
+                wind={wind}
+                images={undefined}
+            />
+            <Settings />
+        </div>
+    );
 };
 
-export default SnowfallPage
+export default SnowfallPage;
