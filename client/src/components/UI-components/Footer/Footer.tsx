7629b6a82839b45faa5f054afedd242a8fdd62ea
Tell396 <vova21473@gmail.com>
2022-11-27 10:36:08 +1000

    Story #78. Update footer.

--- client/src/components/UI-components/Footer/Footer.tsx
+++ client/src/components/UI-components/Footer/Footer.tsx
@@ -1,13 +1,13 @@
 import styles from "./Footer.module.scss";
 
 export const Footer = () => {
-  return (
-    <div>
-      <footer className={styles.footerContainer}>
-        <p className={styles.author}>
-          Made with love by <a href="https://github.com/Tell396">Berezhnev</a>
-        </p>
-      </footer>
-    </div>
-  );
+    return (
+        <div>
+            <footer className={styles.footerContainer}>
+                <p className={styles.author}>
+                    Made with love by <a href="https://berezhnev.vercel.app">Berezhnev</a>
+                </p>
+            </footer>
+        </div>
+    );
 };
