6e72dc293b589f54ba6edda9039619fbc22195e9
Tell396 <vova21473@gmail.com>
2022-11-28 22:03:32 +1000

    Story #81. Update snowfall.

--- client/src/components/screens/Home/Home.tsx
+++ client/src/components/screens/Home/Home.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import * as React from "react";
-import Snowfall from 'react-snowfall'
+import Snowfall from "react-snowfall";
 
 import { NewFilms } from "@/components/screens/Home/components/NewFilms/NewFilms";
 import { NewSeries } from "@/components/screens/Home/components/NewSeries/NewSeries";
@@ -8,15 +8,20 @@ import { NewSeries } from "@/components/screens/Home/components/NewSeries/NewSer
 import styles from "./Home.module.scss";
 
 const Home: NextPage = () => {
-  return (
-			<div className={styles.container}>
-					<Snowfall />
-      <div className={styles.App}>
-        <NewFilms />
-        <NewSeries />
-      </div>
-    </div>
-  );
+    return (
+        <div className={styles.container}>
+            <div className={styles.App}>
+                <Snowfall
+                    snowflakeCount={200}
+                    style={{
+                        height: "230vh",
+                    }}
+                />
+                <NewFilms />
+                <NewSeries />
+            </div>
+        </div>
+    );
 };
 
 export default Home;
