f9edc8980420adf43f9cd07fafdfeeea0e419b0d
Berezhnev Vladimir <vova21473@gmail.com>
2023-01-10 23:41:28 +1000

    Story #104. Starting to refactoring and architecture changing...

--- client/src/components/screens/Home/components/RandomFilms/RandomFilms.tsx
+++ client/src/components/screens/Home/components/RandomFilms/RandomFilms.tsx
@@ -2,16 +2,26 @@ import { IMovie } from "@/types/IMovie";
 import { SwiperSlide } from "swiper/react";
 import { Carousel } from "@/components/Carousel/Carousel";
 import styles from "./RandomFilms.module.scss";
+import { useTypedSelector } from "@/hooks/useTypedSelector";
+import { useGetNewSeriesQuery } from "@/services/KinowayService";
 
 export const RandomFilms = () => {
+    const { filmsLimit } = useTypedSelector((state) => state.loadReducer);
     const { data, isFetching } = useGetNewSeriesQuery(filmsLimit);
-    const randomMovies = data.sort(() => 0.5 - Math.random()).slice(0, 3);
-    const movieIds = randomMovies.map((movie) => movie.id);
+    // const movieIds = randomMovies.map((movie) => movie.id);
+
+    const randomInteger = (min: number, max: number) => {
+        // получить случайное число от (min-0.5) до (max+0.5)
+        let rand = min - 0.5 + Math.random() * (max - min + 1);
+        return Math.round(rand);
+    };
+
+    console.log(randomInteger(342, 507000));
 
     return (
         <div className={styles.container}>
             <Carousel title="Случайное кино" quantity={4}>
-                {movies?.map((data) => {
+                {/* {movies?.map((data) => {
                     console.log(data);
                     console.log("here");
                     return (
@@ -19,7 +29,7 @@ export const RandomFilms = () => {
                             <FilmCard item={data} />
                         </SwiperSlide>
                     );
-                })}
+										})} */}
             </Carousel>
         </div>
     );
