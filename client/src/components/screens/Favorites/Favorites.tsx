1c11913c888817ae821344ecb33fef7cf0da6216
Berezhnev Vladimir <vova21473@gmail.com>
2023-01-15 16:56:50 +1000

    Story #107. Adding a "favorites" pages.

--- client/src/components/screens/Favorites/Favorites.tsx
+++ client/src/components/screens/Favorites/Favorites.tsx
@@ -2,20 +2,32 @@ import { useGetFavoritesQuery } from "@/services/KinowayService";
 import { useTypedSelector } from "@/hooks/useTypedSelector";
 // import { Filters } from '@/components/Filters/Filters';
 import { useFavorites } from "@/hooks/useFavorite";
+import FilmCard from "@/components/FilmCard/FilmCard";
 import Head from "next/head";
 
 export const Favorites = () => {
-  const { favorites } = useFavorites();
-  const { filters } = useTypedSelector((state) => state.filtersReducer);
-  const { page } = useTypedSelector((state) => state.paginationReducer);
-  const query = favorites.map((el) => `search=${el}&field=id`).join("&");
-  const { data, isLoading, isFetching } = useGetFavoritesQuery({
-    page,
-    filters,
-    query,
-  });
+    const { favorites } = useFavorites();
+    const { filters } = useTypedSelector((state) => state.filtersReducer);
+    const { page } = useTypedSelector((state) => state.paginationReducer);
+    const query = favorites.map((el) => `search=${el}&field=id`).join("&");
+    const { data, isLoading, isFetching } = useGetFavoritesQuery({
+        page,
+        filters,
+        query,
+    });
 
-  return <div>213</div>;
+    console.log(data?.docs);
+
+    return (
+        <div>
+            {data?.docs.map((data: any, index: any) => {
+                // console.log(data.name);
+
+                // return <div key={index}>Имя фильма: {data.name}</div>;
+                return <FilmCard item={data} />;
+            })}
+        </div>
+    );
 };
 
 export default Favorites;
--- client/src/pages/favorites.tsx
+++ client/src/pages/favorites.tsx
@@ -1,10 +1,8 @@
-import { Favorites } from '@/components/screens/Favorites/Favorites';
-import { NextPage } from 'next';
+import { Favorites } from "@/components/screens/Favorites/Favorites";
+import { NextPage } from "next";
 
 const FavoritesPage: NextPage = () => {
-    return (
-        <Favorites />
-    );
+    return <Favorites />;
 };
 
 export default FavoritesPage;
