f518128caf6780841fb909d6bb1bd89786e98d62
Berezhnev Vladimir <vova21473@gmail.com>
2023-03-07 13:34:07 +1000

    Story #128. Header was updated.

--- client/src/components/Layout/components/Header/Header.tsx
+++ client/src/components/Layout/components/Header/Header.tsx
@@ -1,14 +1,21 @@
 import styles from "./Header.module.scss";
 import Link from "next/link";
+import { useEffect } from "react";
 import variables from "../../../../styles/variables.module.scss";
 // components
 import { Search } from "@/components/Search/Search";
-import { ThemeSwitcher } from "@/UI/ThemeSwitcher/ThemeSwitcher";
 
 import { RoutesEnum } from "@/constants/routes";
 import { MdFavorite } from "react-icons/md";
+import { Button, useColorMode, useColorModeValue } from "@chakra-ui/react";
+import { MoonIcon, SunIcon, StarIcon, TimeIcon } from "@chakra-ui/icons";
 
 export const Header = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  const bg = useColorModeValue("#313131", "#313131");
+  const color = useColorModeValue("white", "white");
+
   return (
     <div className={styles.headerContainer}>
       <div className={styles.content}>
@@ -24,9 +31,34 @@ export const Header = () => {
         </Link>
 
         <div className={styles.headerRight}>
-          <ThemeSwitcher />
-          <Link href={RoutesEnum.Favourites} className={styles.favorite}>
-            <MdFavorite size="1.6em" />
+          <Button
+            bg={bg}
+            color={color}
+            onClick={() => toggleColorMode()}
+            _hover={{
+              background: "#424242",
+              color: "white",
+            }}
+          >
+            {colorMode === "light" ? (
+              <MoonIcon boxSize={5} />
+            ) : (
+              <SunIcon boxSize={5} />
+            )}
+          </Button>
+          <Link href={RoutesEnum.Favourites}>
+            <Button
+              mr={2}
+              ml={2}
+              bg={bg}
+              color={color}
+              _hover={{
+                background: "#424242",
+                color: "white",
+              }}
+            >
+              <StarIcon boxSize={5} />
+            </Button>
           </Link>
           <Search />
         </div>
@@ -34,3 +66,6 @@ export const Header = () => {
     </div>
   );
 };
+
+// import { ThemeSwitcher } from "@/UI/ThemeSwitcher/ThemeSwitcher";
+// <ThemeSwitcher />
