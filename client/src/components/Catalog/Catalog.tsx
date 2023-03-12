3c4e24acae0cc8351f9263f0beac5093d92e7eb3
Berezhnev Vladimir <vova21473@gmail.com>
2023-03-12 19:45:51 +1000

    Story #143. Minor updates.

--- client/src/components/Catalog/Catalog.tsx
+++ client/src/components/Catalog/Catalog.tsx
@@ -11,97 +11,101 @@ import classNames from "classnames";
 import styles from "./Catalog.module.scss";
 
 interface ContentProps {
-  data: IMovies | undefined;
-  isFetching?: boolean;
-  isLoading?: boolean;
+    data: IMovies | undefined;
+    isFetching?: boolean;
+    isLoading?: boolean;
 }
 
 function Catalog({ children }: PropsWithChildren<{}>) {
-  return <div className={styles.catalog}>{children}</div>;
+    return <div className={styles.catalog}>{children}</div>;
 }
 module Catalog {
-  export const Container = ({ children }: PropsWithChildren<{}>) => {
-    return (
-      <div className={classNames("container wrapper", styles.container)}>
-        {children}
-      </div>
+    export const Container = ({ children }: PropsWithChildren<{}>) => {
+        return (
+            <div className={classNames("container wrapper", styles.container)}>
+                {children}
+            </div>
+        );
+    };
+
+    export const Heading = ({ children }: PropsWithChildren<{}>) => {
+        return <Title className={styles.title}>{children}</Title>;
+    };
+
+    export const Subtitle = ({ children }: PropsWithChildren<{}>) => {
+        return (
     );
-  };
+    };
 
-  export const Heading = ({ children }: PropsWithChildren<{}>) => {
-    return <Title className={styles.title}>{children}</Title>;
-  };
+    export const Description = ({ children }: PropsWithChildren<{}>) => {
+        return <p className={styles.desc}>{children}</p>;
+    };
 
-  export const Subtitle = ({ children }: PropsWithChildren<{}>) => {
-    return (
-      <Title className={styles.subtitle} variant="h2">
-        {children}
-      </Title>
+    export const Loader = () => {
+        return (
+            <div className={styles.spinner}>
+                <Spinner size={SpinnerSizes.medium} />
+            </div>
+        );
+    };
+
+    export const Grid = ({ data }: ContentProps) => {
+        return (
     );
-  };
+    };
 
-  export const Description = ({ children }: PropsWithChildren<{}>) => {
-    return <p className={styles.desc}>{children}</p>;
-  };
-
-  export const Loader = () => {
-    return (
-      <div className={styles.spinner}>
-        <Spinner size={SpinnerSizes.medium} />
-      </div>
+    export const Body = ({ children }: PropsWithChildren<{}>) => {
+        return (
     );
-  };
+    };
 
-  export const Grid = ({ data }: ContentProps) => {
-    return (
-      <div className={styles.grid}>
-        {data?.docs?.map((el) => (
-          <FilmCard key={el.id} item={el} />
-        ))}
-      </div>
-    );
-  };
+    export const Content = ({ data, isLoading, isFetching }: ContentProps) => {
+        const { page } = useTypedSelector((state) => state.paginationReducer);
+        const { setPage } = useActions();
 
-  export const Body = ({ children }: PropsWithChildren<{}>) => {
-    return (
-      <div className={styles.body}>
-        <FiltersToggle />
-        {children}
-      </div>
-    );
-  };
+        useEffect(() => {
+            scrollTo(0, 0);
+        }, [page]);
 
-  export const Content = ({ data, isLoading, isFetching }: ContentProps) => {
-    const { page } = useTypedSelector((state) => state.paginationReducer);
-    const { setPage } = useActions();
+        const CatalogContent = (
+            <>
+                <Catalog.Grid data={data} />
+                <Pagination page={page} setPage={setPage} pages={data?.pages} />
+            </>
+        );
 
-    useEffect(() => {
-      scrollTo(0, 0);
-    }, [page]);
+        return (
+            <>
+                {isLoading || isFetching ? (
+                    <Loader />
+                ) : (
+                    <div>
+                        {!data?.docs?.length ? (
+                            <Subtitle>Ничего не найдено!</Subtitle>
+                        ) : (
+                            CatalogContent
+                        )}
+                    </div>
+                )}
+            </>
+        );
 
-    const CatalogContent = (
-      <>
-        <Catalog.Grid data={data} />
-        <Pagination page={page} setPage={setPage} pages={data?.pages} />
-      </>
-    );
-
-    return (
-      <>
-        {isLoading || isFetching ? (
-          <Loader />
-        ) : (
-          <div className={styles.content}>
-            {!data?.docs?.length ? (
-              <Subtitle>Ничего не найдено!</Subtitle>
-            ) : (
-              CatalogContent
-            )}
-          </div>
-        )}
-      </>
-    );
-  };
+        // return (
+        //   <>
+        //     {isLoading || isFetching ? (
+        //       <Loader />
+        //     ) : (
+        //       <div className={styles.content}>
+        //         {!data?.docs?.length ? (
+        //           <Subtitle>Ничего не найдено!</Subtitle>
+        //         ) : (
+        //           CatalogContent
+        //         )}
+        //       </div>
+        //     )}
+        //   </>
+        // );
+    };
 }
 
 export { Catalog };
--- client/src/components/screens/Films/Films.tsx
+++ client/src/components/screens/Films/Films.tsx
@@ -2,6 +2,7 @@ import { useGetFilmsQuery } from "@/services/KinowayService";
 import { useTypedSelector } from "@/hooks/useTypedSelector";
 import { Catalog } from "@/components/Catalog/Catalog";
 import { Filters } from "@/components/Filters/Filters";
+import { Grid, GridItem } from "@chakra-ui/react";
 
 export const Films = () => {
   const { filters } = useTypedSelector((state) => state.filtersReducer);
@@ -11,15 +12,28 @@ export const Films = () => {
   const { Container, Heading, Description, Body, Content } = Catalog;
 
   return (
-    <Catalog>
-      <Container>
-        <Heading>Все фильмы</Heading>
-        <Description>Подборка фильмов всего мира</Description>
-        <Body>
+    <Body>
+      <Grid templateColumns="repeat(2, 1fr)" gap={6}>
+        <GridItem>
           <Filters />
+        </GridItem>
+        <GridItem>
           <Content data={data} isLoading={isLoading} isFetching={isFetching} />
-        </Body>
-      </Container>
-    </Catalog>
+        </GridItem>
+      </Grid>
+    </Body>
   );
+
+  // return (
+  //   <Catalog>
+  //     <Container>
+  //       <Heading>Все фильмы</Heading>
+  //       <Description>Подборка фильмов всего мира</Description>
+  //       <Body>
+  //         <Filters />
+  //         <Content data={data} isLoading={isLoading} isFetching={isFetching} />
+  //       </Body>
+  //     </Container>
+  //   </Catalog>
+  // );
 };
